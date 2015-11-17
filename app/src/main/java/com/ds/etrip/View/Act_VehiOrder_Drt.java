b9a1a6fafc4fd799576e7bdc6f8f9a508e8265a3
qiyanghong <qiyanghong@qq.com>
2015-11-18 00:29:28 +0800

    新增了5个界面

---
+++ app/src/main/java/com/ds/etrip/View/Act_VehiOrder_Drt.java
@@ -0,0 +1,17 @@
+package com.ds.etrip.View;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ds.etrip.R;
+
+/**
+ * Created by 7YHong on 2015/11/18.
+ */
+public class Act_VehiOrder_Drt extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_vehi_order_drt);
+    }
+}
